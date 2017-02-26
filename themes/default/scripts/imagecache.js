ffb2f2861b26c4cc33c405781173d2d825f37302
spuds <spuds@spudsdesign.com>
2017-02-26 08:01:15 -0600

    ! header

--- themes/default/scripts/imagecache.js
+++ themes/default/scripts/imagecache.js
@@ -1,9 +1,12 @@
 /**
  * Provides a simple image cache, intended for serving https images over https
  *
- * @name      ElkArte Forum
- * @copyright ElkArte Forum contributors
- * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ * @name ImageCache
+ * @author Spuds
+ * @copyright (c) 2017 Spuds
+ * @license This Source Code is subject to the terms of the Mozilla Public License
+ * version 1.1 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/1.1/.
  *
  * @version 1.0.0
  *
