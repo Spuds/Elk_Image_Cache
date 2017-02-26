0cf9176847f2b3d6c4216a654f645f8b095357cc
spuds <spuds@spudsdesign.com>
2017-02-25 18:38:49 -0600

    ! let it install on RC1+ ! fix spelling

--- themes/default/languages/english/ImageCache.english.php
+++ themes/default/languages/english/ImageCache.english.php
@@ -9,14 +9,14 @@ $txt['image_cache_keep_days'] = 'Remove cached images that have not been accesse
 $txt['image_cache_keep_days_subnote'] = 'Enter 0 to keep images indefinitely';
 $txt['image_cache_all'] = 'Cache all [img]\'s, not just ones needed for HTTPS sites.';
 
-$txt['image_cache_desc'] = 'This will serve images embedded with [IMG] tags from your domain through a proxy mechanism.  The remote image is saved to your cache directory and serverd from there.  You can choose to do this for all [IMG] tags or just those that would cause "insecure content" warnings when your site is running HTTPS';
+$txt['image_cache_desc'] = 'This will serve images embedded with [IMG] tags from your domain through a proxy mechanism.  The remote image is saved to your cache directory and served from there.  You can choose to do this for all [IMG] tags or just those that would cause "insecure content" warnings when your site is running HTTPS';
 $txt['image_cache_settings_description'] = 'Here you can set all settings involving the image cache and proxy.';
 
 $txt['maintain_imagecache'] = 'Empty the image cache';
 $txt['maintain_imagecache_info'] = 'This function will EMPTY the image cache should you need it to be cleared.';
 
 $txt['core_settings_item_ic'] = 'Image Cache & Proxy';
-$txt['core_settings_item_ic_desc'] = 'This will serve images embedded with [IMG] tags from your domain through a proxy mechanism.  The remote image is saved to your cache directory and serverd from there.  You can choose to do this for all [IMG] tags or just those that would cause "insecure content" warnings when your site is running HTTPS';
+$txt['core_settings_item_ic_desc'] = 'This will serve images embedded with [IMG] tags from your domain through a proxy mechanism.  The remote image is saved to your cache directory and served from there.  You can choose to do this for all [IMG] tags or just those that would cause "insecure content" warnings when your site is running HTTPS';
 
 $txt['scheduled_task_remove_old_image_cache'] = 'Remove old image cache files';
 $txt['scheduled_task_desc_remove_old_image_cache'] = 'Deletes files older than the number of days defined in the image cache settings in the admin panel.';
